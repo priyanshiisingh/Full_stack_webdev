c85b87129376bf55ed593cf0c88950b156b63bda
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-28 18:56:00 +0530

    datatypes in JS

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-console.log("Hello World");
-console.log("Hello World");
-console.error("Hello World");
+//string
+console.log(typeof "hello world");
+console.log(typeof 'hi');
+
+//number
+console.log(typeof 1);
+console.log(typeof 1.2);
+console.log(typeof - 1);
+console.log(typeof 1234);
+console.log(typeof - 2.1);
+
+//boolean
+console.log(typeof false);
+console.log(typeof true);
