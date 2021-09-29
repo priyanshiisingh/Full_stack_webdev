a2082ef2e6c5af25e55d0aefe2291d4ce14c0ef0
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-29 19:18:06 +0530

    DOM methods

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var para = document.getElementsByClassName('para')
 var tags = document.getElementsByTagName('a')
 var qs = document.querySelector('#heading')
 var qs2 = document.querySelector('.para')
+var qs3 = document.querySelectorAll('.para')
+
 
 console.log(heading)
 console.log(para[0])
 console.log(para[1])
 console.log(para.length)
 console.log(para)
+console.log(tags)
+console.log(qs)
+console.log(qs2)
+console.log(qs3)
 
 //para.push("hello") wont work
 
