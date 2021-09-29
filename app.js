d9ca2034224b38761d5b7d2aee2dfd37e0053646
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-29 19:45:02 +0530

    array and for loops

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-var heading = document.getElementById('heading')
-var body = document.querySelector('body')
+var list = document.querySelector('ul')
+var arr = ['go tot gym', 'eat oatmeal', 'work']
 
-console.log(heading)
-
-heading.innerHTML = "i was added by js "
-heading.style = "color:red; fontsize:5rem;"
-console.log(heading.classList) //shows all the class heading has
-
-heading.classList.add('five')
-heading.classList.remove('one')
-
-console.log(heading.classList)
-
-body.classList.add('dark') //dark mode on
-body.classList.remove('dark'); //dark mode off
+arr.push('complete the assignment')
+console.log(list)
+for (var i = 0; i < arr.length; i++) {
+    var element = document.createElement('li')
+    var textnode = document.createTextNode(arr[i])
+    console.log(element)
+    element.appendChild(textnode)
+    console.log(element)
+    list.appendChild(element)
+}
