18d089e36f2e2412b6a35c6ee1f12e61be3c2526
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-29 19:15:08 +0530

    DOM methods

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 var heading = document.getElementById('heading')
-console.log(typeof heading)
+var para = document.getElementsByClassName('para')
+var tags = document.getElementsByTagName('a')
+var qs = document.querySelector('#heading')
+var qs2 = document.querySelector('.para')
+
+console.log(heading)
+console.log(para[0])
+console.log(para[1])
+console.log(para.length)
+console.log(para)
+
+//para.push("hello") wont work
+
+//getelementbyid returns single object whereas getelementsbyclassname 
+//returns all collection of all the objects of the specified class
