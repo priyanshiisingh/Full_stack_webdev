cc5f419d9acb0493f809943ad6ac0d4b61bcd306
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-30 19:19:04 +0530

    functions

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-var list = document.querySelector('ul')
-var arr = ['go tot gym', 'eat oatmeal', 'work']
+function greet1(name = "john", age = "99") {
+    console.log(name)
+    console.log(age)
 
-arr.push('complete the assignment')
-console.log(list)
-for (var i = 0; i < arr.length; i++) {
-    var element = document.createElement('li')
-    var textnode = document.createTextNode(arr[i])
-    console.log(element)
-    element.appendChild(textnode)
-    console.log(element)
-    list.appendChild(element)
+    console.log("hello");
+}
+
+greet1()
+
+function greet2(a, b) {
+    console.log(a + b)
+
+    console.log("hello");
+}
+
+greet2(10, 12)
+
+function sum(a, b) {
+    var sum = a + b
+    return sum
+}
+
+var result = sum(10, 20)
+console.log(result)
+
+console.log(sum(10, 20))
+
+//function expression
+
+var add = function(a, b) {
+    var sum = a + b
+    return sum;
+}
+
+console.log(add(10, 20))
+
+var greet3 = function(name = 'John', age = 99) {
+
+    var profile = name + age
+    return profile
+
+}
+
+console.log(greet3('rii', 20))
+
+var greet34 = (name = 'John', age = 99) => {
+    console.log(greet34('rii', 20))
+    var profile = name + age
+    return profile
+
 }
