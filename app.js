02fc007afb2b92331b9eec57ee0d5b062a8b3cd4
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-09-30 19:40:36 +0530

    functions foreach

--- app.js
+++ app.js
@@ -1,58 +1,15 @@
-function greet1(name = "john", age = "99") {
-    console.log(name)
-    console.log(age)
+//call back function
+//it means that foreach is implicitly calling the callback function that's y we do not need to call it explicitly?
 
-    console.log("hello");
+
+var arr = ['gym', 'cook', 'eat']
+
+//for (var i = 0; i <= arr.length; i++) {
+//   console.log(arr[i])
+//}
+
+var callbaclfunction = (ele, mystry) => {
+    console.log(ele, mystry)
 }
 
-greet1()
-
-function greet2(a, b) {
-    console.log(a + b)
-
-    console.log("hello");
-}
-
-greet2(10, 12)
-
-function sum(a, b) {
-    var sum = a + b
-    return sum
-}
-
-var result = sum(10, 20)
-console.log(result)
-
-console.log(sum(10, 20))
-
-//function expression
-
-var add = function(a, b) {
-    var sum = a + b
-    return sum;
-}
-
-console.log(add(10, 20))
-
-var greet3 = function(name = 'John', age = 99) {
-
-    var profile = name + age
-    return profile
-
-}
-
-console.log(greet3('rii', 20))
-
-var greet34 = (name = 'John', age = 99) => {
-    console.log(greet34('rii', 20))
-    var profile = name + age
-    return profile
-
-}
-
-var add = (a, b) => a + b
-console.log(add(10, 20))
-
-
-const meet = (name) => 'hi' + name
-console.log(meet("prii"))
+arr.forEach(callbaclfunction)
