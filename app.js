6cbba0816a17b878913d56ba39c49c77b31bbabf
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-01 19:30:57 +0530

    array methods 2

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8]
-    //unshift adds the element in the beginning of the array
-    // shift removes element at index 0
-    //arr.push adds element to the end of the array
 
-//var newarr = arr.slice(2, 5)
-// arr.slice doen't affects the original arr it returns a new edited array
-//arr.shift()
-//console.log(newarr)
+arr.splice(3, 1)
+    //element index and number of elements we want to remove
+    //modifies the original array does not create a new like in slice
+    // slice just removes first elemnet this removes specified index element
 
-//var newarr = []
-
-//for (var i = 0; i <= arr.length, i++;) {
-//    if (arr[i] % 2 === 0) newarr.push(arr[i])
-//}
-
-
-var newarr = arr.filter((element, index) => {
-    if (element % 2 === 0) return true
-})
-
-console.log(newarr)
+console.log(arr)
