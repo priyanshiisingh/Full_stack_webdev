b3a7807d499810ae50a9c5662ff151e38c7875d8
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-01 19:25:58 +0530

    array methods

--- app.js
+++ app.js
@@ -1,72 +1,22 @@
-const a = document.querySelectorAll('input')[0]
-const b = document.querySelectorAll('input')[1]
+var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+    //unshift adds the element in the beginning of the array
+    // shift removes element at index 0
+    //arr.push adds element to the end of the array
 
-const add = document.querySelector('#add')
-const multiply = document.querySelector('#multiply')
-const sub = document.querySelector('#subtract')
+//var newarr = arr.slice(2, 5)
+// arr.slice doen't affects the original arr it returns a new edited array
+//arr.shift()
+//console.log(newarr)
 
-const resultbox = document.querySelector('.result')
+//var newarr = []
 
-//console.log(a, b, add, multiply)
-
-//const sum = () => {
-//    const result = parseInt(a.value) + parseInt(b.value)
-//    resultbox.innerHTML = result
-
-//}
-
-//const multi = () => {
-//    const result = parseInt(a.value) * parseInt(b.value)
-//    resultbox.innerHTML = result
-
+//for (var i = 0; i <= arr.length, i++;) {
+//    if (arr[i] % 2 === 0) newarr.push(arr[i])
 //}
 
 
-const calculate = (event, operation) => {
-    console.log(event.target.value)
-
-    //if (operation == 'add') {
-    //    console.log('adding')
-    //} else if (operation == 'multiply') {
-    //    console.log('multiplying')
-    //}
-
-    switch (operation) {
-        case "add":
-            resultbox.innerHTML = parseInt(a.value) + parseInt(b.value)
-            break
-        case "multiply":
-            resultbox.innerHTML = parseInt(a.value) * parseInt(b.value)
-            break
-        case "subtract":
-            resultbox.innerHTML = parseInt(a.value) - parseInt(b.value)
-            break
-        default:
-            resultbox.innerHTML = "not a valid operation"
-            break
-
-    }
-
-}
-add.addEventListener('click', (event) => {
-        return calculate(event, 'add')
-    })
-    // dummy function returning(calling the actual function) the value 
-    //of calculate function
-    //replies to the click event or else will always be displayed in the
-    //console
-
-//make the dummy function accept the event listner first and then it'll
-// call the parameter for the function
-
-// we were calling a function call instead of its signature 
-
-multiply.addEventListener('click', (event) => {
-    return calculate(event, 'multiply')
+var newarr = arr.filter((element, index) => {
+    if (element % 2 === 0) return true
 })
 
-sub.addEventListener('click', (event) => {
-    return calculate(event, 'subtract')
-})
-
-//we're making a function return another function
+console.log(newarr)
