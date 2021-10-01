a9847081be455f69dce2a044251a0a280da68f23
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-01 19:12:22 +0530

    add , multiply, subd

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const b = document.querySelectorAll('input')[1]
 
 const add = document.querySelector('#add')
 const multiply = document.querySelector('#multiply')
+const sub = document.querySelector('#subtract')
 
 const resultbox = document.querySelector('.result')
 
@@ -23,10 +24,27 @@ const resultbox = document.querySelector('.result')
 
 const calculate = (event, operation) => {
     console.log(event.target.value)
-    if (operation == 'add') {
-        console.log('adding')
-    } else if (operation == 'multiply') {
-        console.log('multiplying')
+
+    //if (operation == 'add') {
+    //    console.log('adding')
+    //} else if (operation == 'multiply') {
+    //    console.log('multiplying')
+    //}
+
+    switch (operation) {
+        case "add":
+            resultbox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "multiply":
+            resultbox.innerHTML = parseInt(a.value) * parseInt(b.value)
+            break
+        case "subtract":
+            resultbox.innerHTML = parseInt(a.value) - parseInt(b.value)
+            break
+        default:
+            resultbox.innerHTML = "not a valid operation"
+            break
+
     }
 
 }
@@ -38,12 +56,17 @@ add.addEventListener('click', (event) => {
     //replies to the click event or else will always be displayed in the
     //console
 
-//
+//make the dummy function accept the event listner first and then it'll
+// call the parameter for the function
 
 // we were calling a function call instead of its signature 
 
 multiply.addEventListener('click', (event) => {
     return calculate(event, 'multiply')
+})
+
+sub.addEventListener('click', (event) => {
+    return calculate(event, 'subtract')
 })
 
 //we're making a function return another function
