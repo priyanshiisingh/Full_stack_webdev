9f5b9b167c65394625b0796fe9e8afb348fed340
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-01 18:59:20 +0530

    add and mutiply dummy functions

--- app.js
+++ app.js
@@ -6,19 +6,44 @@ const multiply = document.querySelector('#multiply')
 
 const resultbox = document.querySelector('.result')
 
-console.log(a, b, add, multiply)
+//console.log(a, b, add, multiply)
 
-const sum = () => {
-    const result = parseInt(a.value) + parseInt(b.value)
-    resultbox.innerHTML = result
+//const sum = () => {
+//    const result = parseInt(a.value) + parseInt(b.value)
+//    resultbox.innerHTML = result
+
+//}
+
+//const multi = () => {
+//    const result = parseInt(a.value) * parseInt(b.value)
+//    resultbox.innerHTML = result
+
+//}
+
+
+const calculate = (event, operation) => {
+    console.log(event.target.value)
+    if (operation == 'add') {
+        console.log('adding')
+    } else if (operation == 'multiply') {
+        console.log('multiplying')
+    }
 
 }
+add.addEventListener('click', (event) => {
+        return calculate(event, 'add')
+    })
+    // dummy function returning(calling the actual function) the value 
+    //of calculate function
+    //replies to the click event or else will always be displayed in the
+    //console
 
-const multi = () => {
-    const result = parseInt(a.value) * parseInt(b.value)
-    resultbox.innerHTML = result
+//
 
-}
+// we were calling a function call instead of its signature 
 
-add.addEventListener('click', sum)
-multiply.addEventListener('click', multi)
+multiply.addEventListener('click', (event) => {
+    return calculate(event, 'multiply')
+})
+
+//we're making a function return another function
