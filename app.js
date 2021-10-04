60163cca7abfd8ad28e076c26c1956c8628b1cbd
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-04 18:31:08 +0530

    Scope var and let

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
-var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
+if (true) {
+    let Name = 'ferrari'
 
-var chores = []
-
-var deleteItem = (value) => {
-    const index = chores.indexOf(value)
-    chores.splice(index, 1)
-    console.log(chores)
 }
+//let and var variables
+//console.log(Name)
 
-//console.log(chores.includes('gym'))
-// checks if entry already in the array
+for (let i = 0; i < 10; i++) {
+    //console.log('loop ran')
 
-const callbackfunc = (event) => {
-    // console.log(input.value)
+}
+//console.log(i)
 
-    const inputValue = input.value
-    if (chores.includes(input.value)) {
-        console.log('already exists')
-    } else {
-        const inputValue = input.value
-        chores.push(inputValue)
-        const element = document.createElement('li')
-        const textNode = document.createTextNode(inputValue)
-        element.appendChild(textNode)
-        list.appendChild(element)
-            //element.addEventListener('click', (e) => { deleteItem(console.log(e.target.innerHTML)) })
-        element.addEventListener('click', (e) => {
-            console.log(e.target.remove)
-        })
+if (true) {
+    console.log(Name)
+    if (true) {
 
+        var Name = 'ferrari'
     }
 }
 
-// to remove the element use e.target.remove
+const print = {} => {
+    var Name = "mercedes"
+    return Name
+}
 
-button.addEventListener('click', callbackfunc)
+print()
