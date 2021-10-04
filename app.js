2a12fbb698e84733dd8986e8b02119ed6d60fe6e
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-04 18:47:53 +0530

    objects

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
-if (true) {
-    let Name = 'ferrari'
-
-}
-//let and var variables
-//console.log(Name)
-
-for (let i = 0; i < 10; i++) {
-    //console.log('loop ran')
-
-}
-//console.log(i)
-
-if (true) {
-    console.log(Name)
-    if (true) {
-
-        var Name = 'ferrari'
+let car = {
+    Name: 'class c',
+    manufacturer: 'mercedes',
+    print: function(a) {
+        console.log(a)
     }
-}
+};
 
-const print = {} => {
-    var Name = "mercedes"
-    return Name
-}
-
-print()
+car.print('ferrari')
+console.log(car.Name)
