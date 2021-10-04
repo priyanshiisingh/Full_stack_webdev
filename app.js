ea691e652e0cb0cc97f0fd9795811149d200e9c9
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-04 19:24:07 +0530

    global Object

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ let car = {
     manufacturer: 'mercedes',
     print: () => {
         console.log(this)
-        console.log(`this is andnkjsa ${this.Name} asnkjdfskhf ${this.manufacturer}`)
+        console.log(`this is andnkjsa ${car.Name} asnkjdfskhf ${car.manufacturer}`)
     }
 };
 
 car.print()
+console.log(car)
+console.log(Object)
+console.log(Object.values(car))
+console.log(Object.keys(car))
+    //
