9dd2ec711cd69950403d8eb6a0bfd2b27b32d80b
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-05 18:49:45 +0530

    call stack, queue and web api

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-//Promises 
-var data = fetch('https://jsonplaceholder.typicode.com/todos/10')
+console.log('one')
+console.log('one2')
 
-console.log(data)
+
+setTimeout(() => {
+    console.log('timer2')
+}, 1000);
+setTimeout(() => {
+    console.log('timer1')
+}, 2000);
+
+
+console.log('one3')
+console.log('one4')
