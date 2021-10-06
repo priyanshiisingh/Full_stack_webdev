5962746e5b35117f4ad0a2d8526f9611b8165555
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-07 03:33:36 +0530

    recipe task js trial-1

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 const input = document.querySelector('input')
 const button = document.querySelector('button')
+let itemname = document.querySelector('h5')
+let itemimage = document.querySelector('img')
+let itemrecipe = document.querySelector('a')
+
+// console.log(itemname, itemimage, imagerecipe)
 
 const item = input.value
 const getdata = async(item) => {
     const url = `https://api.edamam.com/search?app_id=99aea339&app_key=5b5baf66440f0b0d1299647cdc02e53e&q=${item}`
-
     const res = await fetch(url)
     const data = await res.json()
-        // console.log(data.hits)
+
+    data.hits.forEach(() => {
+        let newname = document.createElement('h5')
+        let newnametext = document.createTextNode(`${data.label}`)
+        newname.appendChild(newnametext)
+        itemname.appendChild(newname)
+            // let newimage = document.createElement('img')
+    })
+
+
+
+
+
+    // let newImage = document.createElement('img')
+    // let iimage = document.createTextNode('')
+    // itemimage.src = "https://www.edamam.com/web-img/f62/f62694eabd827755f6c9dcd1e6eb6fff"
+    // itemname.innerHTML = item
+
+
+
+
+    // data.hits.forEach((key) => {
+    //     var ele = document.createElement("div");
+    //     var img = document.createElement("img");
+    //     img.setAttribute("src", data[key]);
+    //     ele.appendChild(img);
+    //     //append ele to parent div
+    // });
+
+
+    // itemrecipe.href=data.url
+    // itemimage.src = `${data.image}`
+
+    // itemimage = data.hits.image
+
+    // itemimage.src = data.image
+    // itemrecipe.innerHTML = data.url
+
+    // data.forEach(image1 => {
+    //     let newImage = document.createElement('img')
+    //     let iimage = `${image1.image}`
+
+    //     newImage.appendChild(iimage)
+    //     itemimage.appendChild(newImage)
+    // })
+
+
+
     data.hits.forEach(recipe => {
         console.log(recipe)
     })
