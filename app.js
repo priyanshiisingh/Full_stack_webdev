19490fe516cfa352310da05311a65b9bd99fdb2e
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-06 19:25:37 +0530

    async await

--- app.js
+++ app.js
@@ -1,64 +1,29 @@
-const tab = document.querySelector('table')
-
-// let promise1 = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("hello world")
-//     }, 3000);
+// let response = fetch('https://jsonplaceholder.typicode.com/users').then((res) => {
+//     let parseddata = res.json()
+//     return (parseddata)
 // }).then((data) => {
 //     console.log(data)
-//
-
-const createrow = ((user) => {
-    let newrow = document.createElement('tr')
-    let template = `
-        <td>${user.id}</td>
-        <td>${user.name}</td>
-        <td>${user.email}</td>
-        <td>${user.username}</td>`
-    newrow.innerHTML = template
-    return newrow
-})
-
-let response = fetch('https://jsonplaceholder.typicode.com/users').then((res) => {
-    let parseddata = res.json()
-    return (parseddata)
-}).then((data) => {
-    console.log(data)
-    data.forEach((user) => {
-        // let newrow = document.createElement('tr')
-        // let template = `
-        // <td>${user.id}</td>
-        // <td>${user.name}</td>
-        // <td>${user.email}</td>
-        // <td>${user.username}</td>`
-        // newrow.innerHTML = template
-        tab.appendChild(createrow(user))
-    })
-})
-
-
+//     data.forEach(user => {
+//         console.log(data)
+//     })
 // })
 
-// let dataid = document.createElement('td')
-//         let dataidtextnode = document.createTextNode(user.id)
-//         dataid.appendChild(dataidtextnode)
-//         newrow.appendChild(dataid)
-//         tab.appendChild(newrow)
-// append_data=(element) => {
-//     const element = document.createElement('li')
-//     userdetail=element['name'] + element['username']
-//     const textnode = document.createTextNode(userdetail)
-//     element.appendChild(textnode)
-//     console.log(element)
-//     list.appendChild(element)
-// }
-// console.log(arr)
+//Async Await
+
+// const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+// const data = await response.json()
+
+// console.log(data)
+
+//output: error
 
 
+const fetchdata = (async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+})
 
-
-// const element = document.createElement('li')
-//     const textnode = document.createTextNode(data)
-//     element.appendChild(textnode)
-//     console.log(element)
-//     list.appendChild(element)
+fetchdata()
