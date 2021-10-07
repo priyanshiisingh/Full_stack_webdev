8d27e8dcf29f3af05fd6d7b3af5f3f36eec82c05
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-08 00:55:53 +0530

    dark mode settings completed

--- assets/js files/todolistjs.js
+++ assets/js files/todolistjs.js
@@ -2,11 +2,10 @@ var dlbutton = document.querySelector('.dl_btn')
 var body = document.querySelector('body')
 
 console.log(dlbutton)
-const callbackfuntion = (() => {
+const dlbutton_callbackfuntion = (() => {
 
     body.classList.toggle('dark')
 
 })
 
-// dlbutton.addEventListener("click", callbackfuntion)
-dlbutton.addEventListener('click', callbackfuntion)
+dlbutton.addEventListener('click', dlbutton_callbackfuntion)
