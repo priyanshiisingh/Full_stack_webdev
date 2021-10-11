73575cb5b9eeb3e8f6fe7729320c2110d9a56311
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-11 19:21:58 +0530

    fs readfilesync

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 
-fs.readFile('./text.txt', 'utf8', (error, data) => {
-    if (error) console.log(error)
-    else console.log(data)
-})
+// fs.readFile('./text.txt', 'utf8', (error, data) => {
+//     if (error) console.log(error)
+//     else console.log(data)
+// })
+
+console.log(fs.readFileSync('./text.txt', 'utf8'))
 
 // fs.readFile('./text.txt', (error, data) => {
 //     if (error) console.log(error)
 //     else console.log(data.toString)
 // })
+
+console.log('abc')
