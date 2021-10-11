deab1364a1b73bb3b63a32c986f295763069510b
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-11 18:39:19 +0530

    path resolve and relatve

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-const div = document.querySelector('div')
+//path module 
 
-const arr = [1, 2, 3, 4, 5]
-localStorage.setItem('val', JSON.stringify(arr))
-console.log(JSON.parse(localStorage.getItem('val')))
+const path = require('path')
+console.log(path.resolve('./app.js'))
 
-
-
-// button.addEventListener('click', (req, res) => {
-//     localStorage.setItem('inputvalue', [1, 2, 3, 4, 5])
-//     div.innerHTML = input.value
-
-// })
-
-// console.log(localStorage.getItem(inputvalue))
+console.log(path.relative('./temp', '././temp/app.js'))
