6a64c6ff012c4c8e7bc411ca33398d062d92e1c6
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-11 19:16:11 +0530

    fs readfile and buffer extra parameters

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 
-fs.readFile('./text.txt', (error, data) => {
+fs.readFile('./text.txt', 'utf8', (error, data) => {
     if (error) console.log(error)
     else console.log(data)
 })
+
+// fs.readFile('./text.txt', (error, data) => {
+//     if (error) console.log(error)
+//     else console.log(data.toString)
+// })
