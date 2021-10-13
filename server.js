f2e1cb060dd3331c2ab66eb89d2bc9799f710a9a
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-13 19:42:36 +0530

    verifying request, middleware, next

--- server.js
+++ server.js
@@ -1,59 +1,38 @@
-// const http = require('http')
-// const fs = require('fs')
-// const path = require('path')
-// console.log(http)
-
-// const server = http.createServer((request, response) => {
-//     // console.log(request.headers)
-//     //console.log(request.url)
-//     // console.log(request.method)
-//     // const path = request.url
-//     const markup = fs.readFileSync(path.resolve('./index.html'))
-//     response.write(markup)
-//     response.end()
-// })
-
-// console.log(server)
-
-// server.listen(3000, () => {
-//     console.log(`Server listening at PORT: ${3000}`)
-// })
-
-// const http = require('http')
-// const fs = require('fs')
-// const path = require('path')
-
-// const server = http.createServer((request, response) => {
-//     // request.url
-//     const { url } = request
-//     if (url == '/login') {
-//         response.write('<h1>LOGIN</h1>')
-//         response.end()
-//     }
-//     if (url == '/signup') {
-//         response.write('<h1>SINGUP</h1>')
-//         response.end()
-//     }
-// })
-
-
-// server.listen(3000, () => {
-//     console.log(`Server listening at PORT: ${3000}`)
-// })
-
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log(req.url)
-    res.send('this is a response')
-})
+// console.log(express)
 
-app.get('/login', (req, res) => {
-    console.log(req.url)
-    res.send('this is a login')
+const verify = (req, res, next) => {
+    if (req.headers['user-agent'] === 'PostmanRuntime/7.28.4') next()
+    else res.send('BLOCKED')
+}
+
+app.get('/', verify, (req, res) => {
+    res.send('verified')
 })
 
 app.listen(3000, () => {
     console.log(`server listening to port`)
 })
+
+
+// module.exports = () => {}
+
+// app.post('/', (req, res) => {
+//     console.log(req.url)
+//     res.send('POST')
+// })
+
+// app.put('/', (req, res) => {
+//     console.log(req.url)
+//     res.send('PUT')
+// })
+// app.patch('/', (req, res) => {
+//     console.log(req.url)
+//     res.send('PATCH')
+// })
+// app.delete('/', (req, res) => {
+//     console.log(req.url)
+//     res.send('Delete')
+// })
