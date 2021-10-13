b03ce55ea2c4447bd40b0456a8fab66bed3e1fe8
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-13 20:14:59 +0530

    app.use, post/signup, body and postman body

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const express = require('express')
 const app = express()
 
-// console.log(express)
+app.use(express.json())
 
-const verify = (req, res, next) => {
-    if (req.headers['user-agent'] === 'PostmanRuntime/7.28.4') next()
-    else res.send('BLOCKED')
-}
+// const isadmin = (req, res, next) => {
+//     if (req.headers.admin === 'true') next()
+//     else res.send('unautorized')
+// }
 
-app.get('/', verify, (req, res) => {
-    res.send('verified')
+app.post('/signup', (req, res) => {
+    console.log(req.body)
+    res.send("dadada")
 })
+
+// app.get('/private', isadmin, (req, res) => {
+//     res.send("I'm a admin route")
+// })
 
 app.listen(3000, () => {
     console.log(`server listening to port`)
 })
+
+// const express = require('express')
+// const PORT = 3000
+// const app = express()
+
+// const isAdmin = (req, res, next) => {
+//     if (req.headers.admin === 'true') next()
+//     else res.send("UNAUTHORISED")
+// }
+
+
+// app.get('/public', (req, res) => {
+//     console.log(req.headers)
+//     res.send(`I'm a public route`)
+// })
+
+// app.get('/private', isAdmin, (req, res) => {
+//     res.send(`I'm a admin route`)
+// })
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at port ${PORT}`)
+// })
 
 
 // module.exports = () => {}
@@ -36,3 +66,11 @@ app.listen(3000, () => {
 //     console.log(req.url)
 //     res.send('Delete')
 // })
+
+
+// console.log(express)
+
+// const verify = (req, res, next) => {
+//     if (req.headers['user-agent'] === 'PostmanRuntime/7.28.4') next()
+//     else res.send('BLOCKED')
+// }
