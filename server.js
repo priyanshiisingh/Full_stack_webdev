00d415df7542b7b7d4fcfc36d8115d32e03f4d0a
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-14 19:25:59 +0530

    get, post, validator

--- server.js
+++ server.js
@@ -3,74 +3,44 @@ const app = express()
 
 app.use(express.json())
 
-// const isadmin = (req, res, next) => {
-//     if (req.headers.admin === 'true') next()
-//     else res.send('unautorized')
-// }
+let products = [{ name: 'iPhone12 Case', price: '999' }, { name: 'iPhone13 Case', price: '1199' }, { name: 'iPhone13 Pro Case', price: '1499' }]
 
-app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send("dadada")
+//-------------------validation middleware--------------
+
+const validator = (req, res, next) => {
+    const { name, price } = req.body
+    if (!name || !price) res.json({ error: 'validation failed' })
+    else next()
+}
+
+
+//-------------------public routes--------------
+//GET route 
+//send arr of products 
+app.get('/products', (req, res) => {
+    res.json({ products })
 })
 
-// app.get('/private', isadmin, (req, res) => {
-//     res.send("I'm a admin route")
-// })
+//-------------------private routes--------------
+app.post('/products/add', validator, (req, res) => {
+    const { name, price } = req.body
+
+    products.push({
+        name,
+        price
+    })
+
+    res.send({ products })
+
+    // const newpro = {
+    //     name,
+    //     price
+    // }
+
+    // products.push(newpro)
+
+})
 
 app.listen(3000, () => {
     console.log(`server listening to port`)
 })
-
-// const express = require('express')
-// const PORT = 3000
-// const app = express()
-
-// const isAdmin = (req, res, next) => {
-//     if (req.headers.admin === 'true') next()
-//     else res.send("UNAUTHORISED")
-// }
-
-
-// app.get('/public', (req, res) => {
-//     console.log(req.headers)
-//     res.send(`I'm a public route`)
-// })
-
-// app.get('/private', isAdmin, (req, res) => {
-//     res.send(`I'm a admin route`)
-// })
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server running at port ${PORT}`)
-// })
-
-
-// module.exports = () => {}
-
-// app.post('/', (req, res) => {
-//     console.log(req.url)
-//     res.send('POST')
-// })
-
-// app.put('/', (req, res) => {
-//     console.log(req.url)
-//     res.send('PUT')
-// })
-// app.patch('/', (req, res) => {
-//     console.log(req.url)
-//     res.send('PATCH')
-// })
-// app.delete('/', (req, res) => {
-//     console.log(req.url)
-//     res.send('Delete')
-// })
-
-
-// console.log(express)
-
-// const verify = (req, res, next) => {
-//     if (req.headers['user-agent'] === 'PostmanRuntime/7.28.4') next()
-//     else res.send('BLOCKED')
-// }
