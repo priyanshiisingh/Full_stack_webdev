3e78b4ec4b6dc92e3b9e595c34f72091c25c72af
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-18 13:35:41 +0530

    try catch error handling

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const database = require('../database/db')
 
-router.get('/all', () => {
+router.get('/all', (req, res) => {
     try {
         res.json({
             categories: database.categories,
--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const app = express()
 const database = require('./database/db')
 app.use(express.json())
 
-app.get('/products/:categories', (req, res) => {
-    if (req.params.categories === 'case') {}
-    if (req.params.categories === 'bands') {}
-    if (req.params.categories === 'phones') {}
 
-    console.log(req.params)
-    res.send('ping')
-})
 
 app.listen(3001, () => {
     console.log(`server listening to port`)
