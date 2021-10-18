e881e35705de847b92a793f65d2dce9a169d3b1c
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-18 19:28:05 +0530

    status and try catch

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,13 +4,13 @@ const database = require('../database/db')
 
 router.get('/all', (req, res) => {
     try {
-        res.json({
+        res.status(200).json({
             categories: database.categories,
             message: "successfully fetched",
             status: "Success"
         })
     } catch (error) {
-        res.json({
+        res.status(200).json({
             categories: [],
             message: error.message,
             status: "Failed"
--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express()
 const database = require('./database/db')
 app.use(express.json())
 
-
+app.get('/', (req, res) => {
+    try {
+        document.getElementById('id')
+    } catch (error) {
+        res.status(200).send(error.message)
+    }
+})
 
 app.listen(3001, () => {
     console.log(`server listening to port`)
