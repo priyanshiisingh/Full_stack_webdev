9d3a6a2c4cd6f5435d487840dd0c2b68ffac4b92
priyanshiisingh <priyanshianamika1815@gmail.com>
2021-10-18 20:02:25 +0530

    router, app.use, uuid, category routes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const database = require('../database/db')
+const { v4: uuidv4 } = require('uuid');
+
 
 router.get('/all', (req, res) => {
     try {
@@ -17,3 +19,30 @@ router.get('/all', (req, res) => {
         })
     }
 })
+
+router.post('/add', (req, res) => {
+    try {
+        const { name } = req.body
+        let newcategory = {
+            name,
+            id: uuidv4()
+        }
+
+        database.categories.push(newcategory)
+
+        res.status(200).json({
+            category: [],
+            message: "succefully fetched",
+            status: "Succes"
+        })
+    } catch (error) {
+        res.status(200).json({
+            category: [],
+            message: error.message,
+            status: "Failed"
+
+        })
+    }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
 const database = require('./database/db')
+const categoryroutes = require('./routes/categoryRoutes')
 app.use(express.json())
+
+console.log(categoryroutes.stack)
+
+// http://3001/categories/route
+// http://3001/mercedes/route
+// http://3001/mercdes/1/1/2/route
+app.use('/categories', categoryroutes)
+
+
+
 
 app.get('/', (req, res) => {
     try {
